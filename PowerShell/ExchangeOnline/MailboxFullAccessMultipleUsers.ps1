16647c42e42046cc3e59a867a6973b551a254960
gmorgan00 <44558722+gmorgan00@users.noreply.github.com>
2024-02-27 11:29:28 +0800

    Update MailboxFullAccessMultipleUsers.ps1

--- PowerShell/ExchangeOnline/MailboxFullAccessMultipleUsers.ps1
+++ PowerShell/ExchangeOnline/MailboxFullAccessMultipleUsers.ps1
@@ -1,1 +1,4 @@
+<# Script to add full mailbox permission for one user to multiple mailboxes #>
+#Connect to Exchange online first
 
+'user1@email.com.au','user2@email.com.au' | Get-Mailbox | Add-MailboxPermission -User userneedingfullaccess@email.com -AccessRights FullAccess -AutoMapping $false3
