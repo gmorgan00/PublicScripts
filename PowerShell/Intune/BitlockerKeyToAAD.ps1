898765d2bb98cfbac369a4ae5a93560c23b73810
gmorgan00 <44558722+gmorgan00@users.noreply.github.com>
2024-02-27 11:26:58 +0800

    Add files via upload

---
+++ PowerShell/Intune/BitlockerKeyToAAD.ps1
@@ -0,0 +1,10 @@
+﻿<#
+This script gets the recovery protector from the OS Drive that with type Recovery Password then
+pushes the recovery password associated with that protector to Azure AD as associated with the
+OS Drive.
+#>
+#Narrow scope to applicable recovery protector
+$AllProtectors = (Get-BitlockerVolume -MountPoint $env:SystemDrive).KeyProtector 
+$RecoveryProtector = ($AllProtectors | where-object { $_.KeyProtectorType -eq "RecoveryPassword" })
+#Push Recovery Passoword AAD
+BackupToAAD-BitLockerKeyProtector $env:systemdrive -KeyProtectorId $RecoveryProtector.KeyProtectorID
