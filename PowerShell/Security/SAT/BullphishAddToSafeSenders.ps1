be29c16ddb58172019c5df9a8dee88dfbad1698c
gmorgan00 <44558722+gmorgan00@users.noreply.github.com>
2024-02-27 11:36:58 +0800

    Update BullphishAddToSafeSenders.ps1

--- PowerShell/Security/SAT/BullphishAddToSafeSenders.ps1
+++ PowerShell/Security/SAT/BullphishAddToSafeSenders.ps1
@@ -1,1 +1,12 @@
+<# Add Phishing simulation senders to safe list in Outlook for BullPhish SAT #>
 
+$users = Get-EXOMailbox
+$senders = "bp-service-support.com","bp-securityawareness.com","online-account.info",
+"myonlinesecuritysupport.com","service-noreply.info","banking-alerts.info","bullphish.com",
+"verifyaccount.help","suspected-fraud.info"
+foreach($user in $users){
+$out = 'Adding Trusted Senders to {0}' -f $user.UserPrincipalName
+Write-Output $out
+Set-MailboxJunkEmailConfiguration $user.UserPrincipalName -TrustedSendersAndDomains @{Add=$senders}
+}
+Write-Output "Finished!"
