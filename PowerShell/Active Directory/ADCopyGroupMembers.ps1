a80954565f6462904dde0823e90255fe0c8d2581
gmorgan00 <44558722+gmorgan00@users.noreply.github.com>
2024-03-18 12:01:20 +0800

    Update ADCopyGroupMembers.ps1

--- PowerShell/Active Directory/ADCopyGroupMembers.ps1
+++ PowerShell/Active Directory/ADCopyGroupMembers.ps1
@@ -1,1 +1,10 @@
+<# Script to copy members from one AD Security Group to another #>
 
+#List group members from first group
+Get-ADGroupMember -Identity "Security Group 1" | Select-Object Name | Sort-Object Name
+
+#Get group members from first group and add to second group
+Get-ADGroupMember -Identity "Security Group 1" | ForEach-Object {Add-ADGroupMember -Identity "Security Group 2" -Members $_.distinguishedName}
+
+#Confirm members added to second group
+Get-ADGroupMember -Identity "Security Group 2" | Select-Object Name | Sort-Object Name
